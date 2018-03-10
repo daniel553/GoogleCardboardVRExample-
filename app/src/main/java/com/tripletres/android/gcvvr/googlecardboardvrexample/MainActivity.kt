7ca7cd37ee25c8138c0bb7e975b51b5b7967971a
Daniel <dany.el553@gmail.com>
2018-03-10 12:42:41 -0600

    Initial commit

---
+++ app/src/main/java/com/tripletres/android/gcvvr/googlecardboardvrexample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tripletres.android.gcvvr.googlecardboardvrexample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
